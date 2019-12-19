9f3e1cbe009bb2171c590647b9676dfa1c29e6f0
Nathan Marimbordes <nmarimbordes@iparla.iutbayonne.univ-pau.fr>
2019-12-19 16:26:32 +0100

    Mise en place de la navbar avec bootstrap fonctionnelle

--- src/Controller/OpenclassdutController.php
+++ src/Controller/OpenclassdutController.php
@@ -7,24 +7,42 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class OpenclassdutController extends AbstractController
 {
+    /**
+     * @Route("/accueil", name="accueil")
+     */
+    
+    
+    
+    
+
     public function accueil()
     {
+        
         return $this->render('openclassdut/accueil.html.twig', [
             'controller_name' => 'OpenclassdutController',
         ]);
     }
+    /**
+     * @Route("/entreprises", name="entreprises")
+     */
     public function entreprises()
     {
         return $this->render('openclassdut/entreprises.html.twig', [
             'controller_name' => 'OpenclassdutController',
         ]);
     }
+    /** 
+     * @Route("/formations", name="formations")
+     */
     public function formations()
     {
         return $this->render('openclassdut/formations.html.twig', [
             'controller_name' => 'OpenclassdutController',
         ]);
     }
+    /** 
+     * @Route("/stage", name="stage")
+     */
     public function stage()
     {
         return $this->render('openclassdut/stage.html.twig');
