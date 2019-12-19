55e76db1f9724f4d4f8c029824c0eeee1d078203
Nathan Marimbordes <nmarimbordes@iparla.iutbayonne.univ-pau.fr>
2019-12-19 18:48:12 +0100

    Génération de plusieurs entreprises aléatoire et envois à la BD

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -9,13 +9,19 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $Lindt = new Entreprise();
-        $Lindt->setAdresse("11 rue avenue Miteran");
-        $Lindt->setActivite("Chocolat");
-        $Lindt->setNom("Lindt");
-        $Lindt->setSite("Lindt.com");
+        $faker = \Faker\Factory::create('fr_FR');
+        $nbEntrep = 10;
+        for ($i=0 ; $i < $nbEntrep ; $i++) { 
+            
+            $Lindt = new Entreprise();
+            $Lindt->setAdresse($faker->realText($maxNbChars = 50 , $indexSize = 2));
+            $Lindt->setActivite($faker->realText($maxNbChars = 50 , $indexSize = 2));
+            $Lindt->setNom($faker->realText($maxNbChars = 30 , $indexSize = 2));
+            $Lindt->setSite($faker->realText($maxNbChars = 50 , $indexSize = 2));
+            
+            $manager->persist($Lindt);
+        }
         
-        $manager->persist($Lindt);
 
         $manager->flush();
     }
