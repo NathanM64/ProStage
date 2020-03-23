905fc42784bd72d3711822cf5e3fe04e5466e310
NathanM64 <marimbordes.nathan@gmail.com>
2020-03-23 18:13:52 +0100

    Ajout du formulaire d'inscription + enregistrement en BD de nouveaux utilisateurs

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,7 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Form\UserType;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class SecurityController extends AbstractController
 {
     /**
@@ -31,6 +36,32 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
-        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+    }
+
+    public function inscription(Request $requetteHttp, ObjectManager $manager, UserPasswordEncoderInterface $encoder)
+    {
+        $user = new User();
+
+        $formulaireUser = $this -> createForm(UserType::class, $user);
+
+        $formulaireUser->handleRequest($requetteHttp);
+
+        if($formulaireUser->isSubmitted() && $formulaireUser->isValid())
+        {
+            //Ajouter roles à l'utilisateur
+            $user->setRoles(['ROLE_USER']);
+
+            //Encoder mot de passe
+            $encodagePassword = $encoder->encodePassword($user, $user->getPassword());
+
+            $user->setPassword($encodagePassword);
+
+            //Enregistrer en BD
+            $manager->persist($user);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+        return $this->render('security/inscription.html.twig', ['vueFormulaireInscription' => $formulaireUser->createView()]);
     }
 }
