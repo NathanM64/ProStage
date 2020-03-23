cc79bfc4a6731a9920b4ce501790f8ebf620beab
NathanM64 <marimbordes.nathan@gmail.com>
2020-03-23 16:10:52 +0100

    Ajout de deux utilisateur + gestion des accés de différentes pages

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,10 +7,29 @@ use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Entreprise;
 use App\Entity\Formations;
 use App\Entity\Stage;
+use App\Entity\User;
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        //Création d'un Utilisateur admin
+
+        $nathan = new User();
+        $nathan ->setPrenom("Nathan");
+        $nathan ->setNom("Marimbordes");
+        $nathan ->setEmail("marimbordes.nathan@gmail.com");
+        $nathan ->setRoles(['ROLE_USER','ROLE_ADMIN']);
+        $nathan ->setPassword('$2y$10$uembXr0po.FF1UYp4QlBVenhEOuHIWhvddjuXh4EH33I4dyWHwwtq');
+
+        $manager->persist($nathan);
+        $user = new User();
+        $user ->setPrenom("u");
+        $user ->setNom("u");
+        $user ->setEmail("u@gmail.com");
+        $user ->setRoles("ROLE_USER");
+        $user ->setPassword('$2y$10$uembXr0po.FF1UYp4QlBVenhEOuHIWhvddjuXh4EH33I4dyWHwwtq');
+
+        $manager->persist($user);
         $faker = \Faker\Factory::create('fr_FR');
         
         for ($i=0 ; $i < 10 ; $i++) { 
